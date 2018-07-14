6e03bf9dd017b49efcb14fd57fc91409ebe4b2c2
Uday1601 <udaykumarg1601@gmail.com>
2018-07-14 07:28:46 +0530

    updated app

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <body>
 
 <h1>Changed the code!!</h1>
-  <h2> required approval for this commit <h2>
+  <h2> required approval for this commit </h2>
+    <h3> updated</h3>
 
 <?php
 echo "version 2.0";
